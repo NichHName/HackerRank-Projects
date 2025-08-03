c373a440de69622ccbf7e9487abd95d28f9c13ef
NicholasH <nwilsonh1201@gmail.com>
2025-08-03 18:40:14 -0400

    Create basic_data_types.cpp

---
+++ cpp/basic_data_types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    
+    char c;
+    double lf;
+    int d;
+    float f;
+    long ld;
+    
+
+    scanf("%d %ld %c %f %lf", &d, &ld, &c, &f, &lf);
+    
+    printf("%d\n%ld\n%c\n%f\n%lf", d, ld, c, f, lf);
+    
+    return 0;
+}
