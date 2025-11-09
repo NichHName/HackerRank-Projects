822608aee963ac413319d09c908949c7ac6fc7ae
Nicholas Harsell <nwilsonh1201@gmail.com>
2025-11-09 17:24:04 -0500

    Create stringstream.cpp
    Implement parseInts function to convert comma-separated string to vector of integers.

---
+++ cpp/stringstream.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    char ch = ',';
+    string a;
+    vector<int> ints = {};
+    while (getline(ss, a, ch)) {
+        ints.push_back(stoi(a));
+    }
+    
+    return ints;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
